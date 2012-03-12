38429fd81a83a0017bd6a040fdef26fd816f48ff
Jason Yosinski <jason@yosinski.com>
2012-03-12 19:39:48 +0000

    smaller image, fixed 404/500 handler bug, added google analytics

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ import os
 import random
 
 from django.shortcuts import render_to_response, redirect
+from django.template.loader import render_to_string
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseServerError
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
@@ -31,3 +32,13 @@ def index(request):
 
     return render_to_response('main/index.html',
                               context_instance = RequestContext(request))
+
+
+
+def handler404(request):
+    return HttpResponseNotFound(render_to_string('main/404.html',
+                                                 context_instance = RequestContext(request)))
+
+def handler500(request):
+    return HttpResponseServerError(render_to_string('main/500.html',
+                                                    context_instance = RequestContext(request)))
